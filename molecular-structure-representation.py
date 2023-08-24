df6b350e74160c0c126403a5ad89963c224fa9ed
Pasha Mango <141678664+pashamango@users.noreply.github.com>
2023-08-23 20:00:37 -0700

    Create molecular-structure-representation.py

---
+++ molecular-structure-representation.py
@@ -0,0 +1,29 @@
+class MolecularStructure:
+    def __init__(self, atoms, bonds):
+        self.atoms = atoms
+        self.bonds = bonds
+
+    def get_information(self):
+        info = f"The molecular structure contains {len(self.atoms)} atoms: {', '.join(self.atoms)}."
+        info += f"\nBonds: {', '.join([f'{bond[0]}-{bond[1]}' for bond in self.bonds])}."
+        return info
+
+    def serialize(self):
+        return {'atoms': self.atoms, 'bonds': self.bonds}
+
+    @staticmethod
+    def deserialize(serialized_structure):
+        return MolecularStructure(serialized_structure['atoms'], serialized_structure['bonds'])
+
+
+def create_structure(atoms, bonds):
+    return MolecularStructure(atoms, bonds)
+
+
+# Example usage
+if __name__ == "__main__":
+    structure = create_structure(["H", "O", "H"], [("H", "O"), ("O", "H")])
+    print(structure.get_information())
+    serialized = structure.serialize()
+    deserialized_structure = MolecularStructure.deserialize(serialized)
+    print(deserialized_structure.get_information())
